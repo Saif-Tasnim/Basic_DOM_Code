2c668c8bcae62f03383500c98a16c73f901bce4a
Saif-Tasnim <saiftasnim2002@gmail.com>
2023-02-09 13:35:20 +0600

    5 important buil in method updated fully

--- js/dem.js
+++ js/dem.js
@@ -16,3 +16,21 @@ console.log(heading[0].innerText);
 console.log(document.getElementsByTagName('ul'));
 const ulCollection = document.getElementsByTagName('ul');
 console.log(ulCollection[0].innerHTML)
+
+
+// Query selector
+console.log("---------------------------------------------------")
+console.log("                Query Selector")
+console.log("---------------------------------------------------")
+
+console.log(document.querySelector("#fruits-title")); // only for first item
+console.log(document.querySelector(".place-name"));  // only for first item
+
+const class_selector = document.querySelectorAll('.place-name');
+console.log(class_selector)
+const id_selector = document.querySelectorAll('#fruits-title');
+console.log(id_selector)
+
+console.log("---------------------------------------------------")
+console.log("                Inside In HTML File Js Start")
+console.log("---------------------------------------------------")
